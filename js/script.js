4c060bee8f1ff905b120a7af15325bd8c51d095f
Jacob Humphreys <jacob_humphreys@icloud.com>
2024-11-13 19:41:22 -0700

    dynamic canvas size with window size and color picker added.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-let myCanvas = document.getElementById('myCanvas');
-let ctx = myCanvas.getContext('2d');
-
-myCanvas.width = 1100;
-myCanvas.height = 650;
-
-
 const circle1 = new PtRCircle(new Coor(0,0), 100);  
 const ctrl_pt = new Coor(0,5);
 const midLine = new LineSegment(circle1.origin, ctrl_pt);
@@ -13,15 +6,6 @@ const targetPt2 = new Coor(100,100);
 
 const threshold = 15; 
 const easingFactor = .010;
-
-ctx.translate(myCanvas.width / 2, myCanvas.height / 2);
-ctx.scale(1, -1);
-
-// document.getElementById("minY").innerText = `Y -${myCanvas.height / 2}`;
-// document.getElementById("maxY").innerText = `Y +${myCanvas.height / 2}`;
-// document.getElementById("minX").innerText = `X -${myCanvas.width / 2}`;
-// document.getElementById("maxX").innerText = `X +${myCanvas.width / 2}`;
-
 
 function handlePtInCircle(color) {
     let tanPair = new TangentPair(circle1, ctrl_pt);
@@ -32,41 +16,45 @@ function handlePtInCircle(color) {
     // let c2tanB = new LineSegment(circle1.origin, tanPair.p2);
     let a2bLine = new LineSegment(tanPair.p1, tanPair.p2);
     
-    tanLine1.draw(ctx, {color : "red"});
-    tanLine2.draw(ctx, {color : "red"});
-    // c2tanA.draw(ctx, {color : "red"});
-    // c2tanB.draw(ctx, {color : "red"});
-    a2bLine.draw(ctx, {color : "red", dashed : true})
+    tanLine1.draw(ctx, {color : `${color}`})
+    tanLine2.draw(ctx, {color : `${color}`})
+    // c2tanA.draw(ctx, {color : `${color}`})
+    // c2tanB.draw(ctx, {color : `${color}`})
+    a2bLine.draw(ctx, {color : `${color}`, dashed: true})
 }
 
 function updateCanvas() {
-    ctx.clearRect(-myCanvas.width/2, -myCanvas.height/2, myCanvas.width, myCanvas.height);
-    
-    let color = 'rgb(100,125,50)'
-    
-    let cPCircle = new CPCircle(circle1.origin, targetPt, ctrl_pt);
-    cPCircle.draw(ctx, {color : `${color}`});
-    
-    if (pt2pt_dist(targetPt, targetPt2) < threshold) { 
-        do {
-            targetPt2.x = Math.random() * myCanvas.width - myCanvas.width / 2;
-            targetPt2.y = Math.random() * myCanvas.height - myCanvas.height / 2;
-        } while (pt2pt_dist(targetPt2,circle1.origin) <= circle1.r + 15 || pt2pt_dist(targetPt2, circle1.origin) >= circle1.r + 200)
-        }
+    if (!paused) {
 
+        ctx.clearRect(-myCanvas.width/2, -myCanvas.height/2, myCanvas.width, myCanvas.height);
+        
+        let color = colorInput.value;
+        
+        let cPCircle = new CPCircle(circle1.origin, targetPt, ctrl_pt);
+        cPCircle.draw(ctx, {color : `${color}`});
+        
+        if (pt2pt_dist(targetPt, targetPt2) < threshold) { 
+            do {
+                targetPt2.x = Math.random() * myCanvas.width - myCanvas.width / 2;
+                targetPt2.y = Math.random() * myCanvas.height - myCanvas.height / 2;
+            } while (pt2pt_dist(targetPt2,circle1.origin) <= circle1.r + 15 || pt2pt_dist(targetPt2, circle1.origin) >= circle1.r + 200)
+        }
+    
         targetPt.x += (targetPt2.x - targetPt.x) * easingFactor
         targetPt.y += (targetPt2.y - targetPt.y) * easingFactor
         ctrl_pt.x += (targetPt.x - ctrl_pt.x) * easingFactor;
         ctrl_pt.y += (targetPt.y - ctrl_pt.y) * easingFactor;
-        
+            
         if (pt2pt_dist(circle1.origin, ctrl_pt) >= circle1.r) {
             handlePtInCircle(color)
         }
-        circle1.draw(ctx, color);
-        ctrl_pt.draw(ctx, color);
-        targetPt.draw(ctx, color);
-        targetPt2.draw(ctx, color);
-        midLine.draw(ctx, color);
+        
+        circle1.draw(ctx, {color : `${color}`});
+        ctrl_pt.draw(ctx, {color : `${color}`});
+        targetPt.draw(ctx, {color : `${color}`});
+        targetPt2.draw(ctx, {color : `${color}`});
+        // midLine.draw(ctx, {color : `${color}`});
+    }
 }
 
 function animate() {
